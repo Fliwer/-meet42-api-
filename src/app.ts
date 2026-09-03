092a449d9c9d6c05335da7f898a4b2d73603910d
Fliwer <p.morthier@gmail.com>
2026-09-03 11:19:05 +0200

    Connexion à PostgreSQL via TypeORM

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import { AppDataSource } from './data_source';
 
 const app = express();
+const PORT = process.env.PORT!;
 
-app.listen(8080, () => {
-  console.log('🚀 Express API launched on port 8080');
-  console.log('http://localhost:8080/');
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log('📦 Connexion à la base de données réussie');
+
+    app.listen(PORT, () => {
+      console.log('🚀 Express API launched on port ' + PORT);
+      console.log('http://localhost:' + PORT + '/');
+    });
+  })
+  .catch((error) => {
+    console.error('❌ Erreur de connexion à la base de données', error);
+  });
